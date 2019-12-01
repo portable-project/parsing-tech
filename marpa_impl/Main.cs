b07ba553ec61d7fbc757896629355870bcdf078d
paekva <paekva@yandex.ru>
2019-12-01 16:01:52 +0300

    added first impl of Grammar object

---
+++ marpa_impl/Main.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace marpa_impl
+{
+    public class Main
+    {
+
+    }
+}
---
+++ marpa_impl/MarpaObj/Grammar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace marpa_impl
+{
+    class Grammar
+    {
+        private readonly List<Symbol> ExtSymList;
+        private readonly List<Symbol> IntSymList;
+
+        public Grammar()
+        {
+            ExtSymList = new List<Symbol>();
+            IntSymList = new List<Symbol>();
+        }
+
+        public int GetExtSymListSize()
+        {
+            return ExtSymList.Count;
+        }
+        public Symbol GetExtSymById(int ExtSymId)
+        {
+            return ExtSymList[ExtSymId];
+        }
+        public void AddExtSym(Symbol ExtSym)
+        {
+            ExtSymList.Add(ExtSym);
+        }
+        public bool IsExtSymIdValid(int ExtSymId)
+        {
+            return ExtSymId >= 0 && ExtSymId < ExtSymList.Count;
+        }
+
+        public int GetIntSymListSize()
+        {
+            return IntSymList.Count;
+        }
+        public Symbol GetIntSymById(int IntSymId)
+        {
+            return IntSymList[IntSymId];
+        }
+        public void AddIntSym(Symbol IntSym)
+        {
+            IntSymList.Add(IntSym);
+        }
+        public bool IsIntSymIdValid(int IntSymId)
+        {
+            return IntSymId >= 0 && IntSymId < IntSymList.Count;
+        }
+    }
+}
