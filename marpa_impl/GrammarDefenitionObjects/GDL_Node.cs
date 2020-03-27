b4ac25519db467906ded8beb45b34cb524daec48
paekva <paekva@yandex.ru>
2020-03-27 09:13:47 +0300

    added lexem tree creation

---
+++ marpa_impl/GrammarDefenitionObjects/GDL_Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace marpa_impl.GrammarDefenitionObjects
+{
+    class GDL_Node
+    {
+        private GDL_Type _type;
+        private string _value;
+        private List<GDL_Node> _children;
+
+        public GDL_Node(GDL_Type type, string value, List<GDL_Node> children)
+        {
+            _type = type;
+            _value = value;
+            _children = children;
+        }
+    }
+}
--- marpa_impl/GrammarDefenitionObjects/GDL_Processor.cs
+++ marpa_impl/GrammarDefenitionObjects/GDL_Processor.cs
@@ -10,7 +10,7 @@ namespace marpa_impl.GrammarDefenitionObjects
         public void TryProcessGrammarDefenition(List<string> files)
         {
             int mainFileIndex = GetMainGrammarDefenitionDocument(files);
-            GetOuterDefenitionStructure(files[mainFileIndex], GDL_Type.RULE_SET);
+            GDL_Node node = GetOuterDefenitionStructure(files[mainFileIndex], GDL_Type.RULE_SET);
         }
 
         private int GetMainGrammarDefenitionDocument(List<string> files)
@@ -19,30 +19,32 @@ namespace marpa_impl.GrammarDefenitionObjects
             return 0;
         }
 
-        private void GetOuterDefenitionStructure(string input, GDL_Type type)
+        private GDL_Node GetOuterDefenitionStructure(string input, GDL_Type type)
         {
+            List<GDL_Node> children = new List<GDL_Node>();
+
             GDL_Item languageItem = GrammarDefenitionLanguage.GetLanguageItemByType(type);
             if(languageItem.GetRegex() != null)
             {
                 GroupCollection x = GetTokenValue(input, languageItem.GetRegex());
-                if(languageItem.GetTokenList() == null)
+                if (languageItem.GetTokenList() != null)
                 {
-                    // TODO: construct some tree of lexems
-                    return;
-                }
-                languageItem.GetTokenList().ForEach(token =>
-                {
-                    string tokenRep = token.ToString();
-                    string inputPart = x[tokenRep].Value;
-                    if (inputPart.Length > 0)
+                    languageItem.GetTokenList().ForEach(token =>
                     {
-                        Console.WriteLine(tokenRep);
-                        Console.WriteLine(inputPart);
-                        Console.WriteLine("---------------------\n");
-                        GetOuterDefenitionStructure(inputPart, token);
-                    }
-                });
+                        string tokenRep = token.ToString();
+                        string inputPart = x[tokenRep].Value;
+                        if (inputPart.Length > 0)
+                        {
+                            Console.WriteLine(tokenRep);
+                            Console.WriteLine(inputPart);
+                            Console.WriteLine("---------------------\n");
+                            children.Add(GetOuterDefenitionStructure(inputPart, token));
+                        }
+                    });
+                }
             }
+
+            return new GDL_Node(type, input, children);
         }
 
         private GroupCollection GetTokenValue(string str, string pattern)
